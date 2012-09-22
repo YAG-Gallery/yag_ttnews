6d31a343e0b4a4aa06a7e295b5051ebc3a20cadb
Michael Knoll <mimi@kaktusteam.de>
2012-09-22 22:09:46 +0200

    ADD: Added template for flexform for tt_news form in backend. This flexform now differs from flexform used by yag in content elements. FIX: Fixed rendering of flexform in albumselector

--- class.tx_yagttnews_albumselector.php
+++ class.tx_yagttnews_albumselector.php
@@ -35,9 +35,65 @@ class tx_yagttnews_albumselector extends user_Tx_Yag_Utility_Flexform_RecordSele
 
 	public function userAlbumSelectorRenderer($PA, $fobj) {
 
-		return $this->renderSourceSelector($PA, $fobj) . ' ' . $this->renderSelectedAlbum($PA, $fobj);
+		/**
+		 * So this is how it works:
+		 *
+		 * renderSourceSelector renders the 3 col box for gallery, album, item
+		 * if a gallery is selected, this is passed to the FLUID template and the selected gallery is rendered in first col
+		 * selected gallery has to be passed to HTML output in hidden fields, as ajax reloads selected album
+		 * that's why we have to use renderSelectedGallery and renderSelectedAlbum
+		 */
+		return $this->renderTtnewsSourceSelector($PA, $fobj) . ' ' .
+				$this->renderSelectedAlbum($PA, $fobj);
+
+	}
 
 
+
+	/**
+	 * Render a source selector to select gallery / album / item at once
+	 *
+	 * @param unknown_type $PA
+	 * @param unknown_type $fobj
+	 */
+	public function renderTtnewsSourceSelector(&$PA, &$fobj) {
+		$this->determineCurrentPID($PA['row']['pid']);
+		$this->init();
+
+		$template = t3lib_div::getFileAbsFileName('EXT:yag_ttnews/Resources/Private/Templates/Backend/FlexForm/FlexFormSource.html');
+		$renderer = $this->getFluidRenderer();
+
+		$renderer->setTemplatePathAndFilename($template);
+
+		$galleryRepository = $this->objectManager->get('Tx_Yag_Domain_Repository_GalleryRepository'); /* @var $galleryRepository Tx_Yag_Domain_Repository_GalleryRepository */
+		$galleries = $galleryRepository->findAll();
+
+		$selectedAlbumUid = intval($PA['row']['tx_yagttnews_album_uid']);
+
+		if($selectedAlbumUid) {
+
+			$albumRepository = $this->objectManager->get('Tx_Yag_Domain_Repository_AlbumRepository'); /* @var $albumRepository Tx_Yag_Domain_Repository_AlbumRepository */
+			$selectedAlbum = $albumRepository->findByUid($selectedAlbumUid); /* @var $selectedAlbum Tx_Yag_Domain_Model_Album */
+			$selectedGallery = $selectedAlbum->getGallery();
+
+			if($selectedAlbum) {
+				$renderer->assign('selectedAlbum', $selectedAlbum);
+				$renderer->assign('selectedGallery', $selectedGallery);
+				$renderer->assign('selectedGalleryUid', $selectedGallery->getUid());
+
+				$renderer->assign('albums', $selectedGallery->getAlbums());
+				$renderer->assign('images', $selectedAlbum->getItems());
+			}
+		}
+
+		$renderer->assign('galleries', $galleries);
+		$renderer->assign('PA', $PA);
+
+		$content = $renderer->render();
+
+		$this->extbaseShutdown();
+
+		return $content;
 	}
 	
 }
