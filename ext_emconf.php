c5bfa8de6bf7256eb86afae39a40e2fbc7ce4508
Daniel Lienert <daniel@lienert.cc>
2012-11-24 20:41:10 +0100

    TASK: uploaded to TER as version 0.4.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "yag_ttnews".
 #
-# Auto generated 22-09-2012 22:30
+# Auto generated 24-11-2012 20:40
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'YAG integration for tt_news. Enables inserting albums into news records.',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '0.3.0-dev',
+	'version' => '0.4.0',
 	'dependencies' => 'cms,extbase,fluid,yag,tt_news',
 	'conflicts' => '',
 	'priority' => '',
@@ -45,7 +45,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:16:{s:36:"class.tx_yagttnews_albumrenderer.php";s:4:"2639";s:36:"class.tx_yagttnews_albumselector.php";s:4:"4645";s:12:"ext_icon.gif";s:4:"fef0";s:17:"ext_localconf.php";s:4:"e078";s:14:"ext_tables.php";s:4:"e580";s:14:"ext_tables.sql";s:4:"7703";s:38:"Configuration/TypoScript/constants.txt";s:4:"9e06";s:34:"Configuration/TypoScript/setup.txt";s:4:"b6bf";s:40:"Resources/Private/Language/locallang.xml";s:4:"aba7";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"49d2";s:42:"Resources/Private/Partials/ImageThumb.html";s:4:"8a2d";s:37:"Resources/Private/Partials/Pager.html";s:4:"6134";s:64:"Resources/Private/Templates/Backend/FlexForm/FlexFormSource.html";s:4:"4dc0";s:46:"Resources/Private/Templates/ItemList/List.html";s:4:"f397";s:31:"Resources/Public/CSS/styles.css";s:4:"ca0d";s:14:"doc/manual.sxw";s:4:"cd94";}',
+	'_md5_values_when_last_written' => 'a:16:{s:36:"class.tx_yagttnews_albumrenderer.php";s:4:"1ca3";s:36:"class.tx_yagttnews_albumselector.php";s:4:"61ef";s:12:"ext_icon.gif";s:4:"fef0";s:17:"ext_localconf.php";s:4:"e078";s:14:"ext_tables.php";s:4:"e580";s:14:"ext_tables.sql";s:4:"7703";s:38:"Configuration/TypoScript/constants.txt";s:4:"9e06";s:34:"Configuration/TypoScript/setup.txt";s:4:"b6bf";s:40:"Resources/Private/Language/locallang.xml";s:4:"aba7";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"49d2";s:42:"Resources/Private/Partials/ImageThumb.html";s:4:"8a2d";s:37:"Resources/Private/Partials/Pager.html";s:4:"6134";s:64:"Resources/Private/Templates/Backend/FlexForm/FlexFormSource.html";s:4:"71a9";s:46:"Resources/Private/Templates/ItemList/List.html";s:4:"f397";s:31:"Resources/Public/CSS/styles.css";s:4:"ca0d";s:14:"doc/manual.sxw";s:4:"f41a";}',
 	'suggests' => array(
 	),
 );
